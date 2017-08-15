f78c18519019d8be8ddcdfecf58676156a7f536d
kajsa kristersson <k.kristersson@gmail.com>
2017-08-15 17:27:17 +0200

    views and controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/tasks", to: "tasks#index" # display all tasks
+  # get "/tasks/new", to: "tasks#new" # render a form to create a new task
+  # get "/tasks/:id", to: "tasks#show" # get a specific task
+  # post "/tasks", to: "tasks#create" # create new task
+  # get "/tasks/:id/edit", to: "tasks#edit" # get form of an existing task
+  # patch "/tasks/:id", to: "tasks#update" # update an existing task
+  # delete "/tasks/:id", to: "tasks#destroy" # deleta a task
+  resources :tasks
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(name: "Pick up books", description: "at library")
+Task.create(name: "Wash dishes", description: "plates and cups")
+Task.create(name: "Workout", description: "run in the park")
